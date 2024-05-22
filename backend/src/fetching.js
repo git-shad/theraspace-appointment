5b869fab6002885fd0084449b069baf0fa8af51d
git-shad <isaacnievarez@gmail.com>
2024-05-23 07:13:25 +0800

    update

--- backend/src/fetching.js
+++ backend/src/fetching.js
@@ -323,6 +323,39 @@ if (view_history) {
     })
 }
 
+function deleteHistory(id){
+    Swal.fire({
+          title: "Do you want to proceed?",
+          text: 'Delete History',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes'
+      }).then((result) => {
+
+        fetch(`/dashboard/history/${id}`,{
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'}
+        })
+        .then(response => response.json())
+        .then(data => {
+            if(result.isConfirmed && data.success === 'true'){
+                Swal.fire({
+                   title: "Deleted Successfully!",
+                   text: "Recorded",
+                   icon: "success"
+                });
+                
+              }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+          
+      })
+  }
+
 /*
     END
 */
